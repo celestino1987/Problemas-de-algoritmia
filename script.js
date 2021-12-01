38c73d84226b10ec066571e5354fad2544626145
Jesús prada <jesusprada95@gmaal.com>
2021-12-01 10:44:21 +0100

    Primer ejercicio de algoritmia

---
+++ script.js
@@ -0,0 +1,39 @@
+const arrExit = new Array();
+const data = [
+  [0, " "],
+  [2, "A", "B", "C"],
+  [3, "D", "E", "F"],
+  [4, "G", "H", "I"],
+  [5, "J", "K", "L"],
+  [6, "M", "N", "O"],
+  [7, "P", "Q", "R", "S"],
+  [8, "T", "U", "V"],
+  [9, "W", "X", "Y", "Z"],
+];
+
+const handleText = (text) => {
+  const arrText = [...text.toUpperCase()];
+  //Cojo las letras del texto una por una
+  arrText.forEach((lyrics) => {
+    //cojo los array del data
+    data.forEach((num) => {
+      //miro si la letra que le paso esta incluoida dentro de cada array que saque en el paso anterior
+      if (num.includes(lyrics)) {
+        //si existe que me de el numero de ese array donde contenga esa letra
+        num[0];
+
+        //cojo el indice de la letra
+        num.indexOf(lyrics);
+        //repito el nupero segun el indice
+        const numRepeat = num[0].toString().repeat(num.indexOf(lyrics));
+        arrExit.push(numRepeat);
+      }
+    });
+  });
+
+  const newString = arrExit.reduce((a, b) => a + b);
+
+  console.log("Texto de entrada ==>", text);
+  console.log("Salida ===>", newString.replace(/0/g, " "));
+};
+handleText("hay lo tienes payaso  de mierda");
